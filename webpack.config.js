9c2fa7395aac05808148de782d2ef654af1bd919
Olivier Blangéro <olivier@lidem.eu>
2020-10-22 09:56:45 +0200

    Ajout de Babel pour gérer les syntaxes modernes des classes

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,49 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Heritage JS Objet',
-      template: 'src/index.ejs'
-    }),
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
-  module: {
-    rules: [
-      // Traitement des SCSS et CSS
-      {
-        test: /\.s?css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      // Traitement des images
-      {
-          test: /\.(png|gif|jpg|jpeg|svg)$/i,
-          use: [ 'file-loader' ]
-      }
-    ]
-  }
+    entry: './src/index.js',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Heritage JS Objet',
+            template: 'src/index.ejs'
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            // Traitement des SCSS et CSS
+            {
+                test: /\.s?css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            // Traitement des images
+            {
+                test: /\.(png|gif|jpg|jpeg|svg)$/i,
+                use: ['file-loader']
+            }
+        ]
+    }
 };
