e51fd7fe18ed120c9698133debb838858b311d57
Max6613 <maxime66110@gmail.com>
2020-11-03 12:13:16 +0100

    Initialisation de la carte

--- src/App/App.js
+++ src/App/App.js
@@ -9,7 +9,26 @@ import '../Styles/style.scss';
 
 
 class App {
+    map;
 
+    constructor() {
+        mapboxgl.accessToken = config.apis.mapbox_gl.api_key;
+    }
+
+    start() {
+        console.log( 'Application démarrée' );
+
+        //Définition du centre de la carte à afficher
+        const mapCenter = new mapboxgl.LngLat( 2.213749, 47.038165 );
+
+        //Instanciation de la carte
+        this.map = new mapboxgl.Map({
+            container: 'map',
+            style: config.apis.mapbox_gl.map_style,
+            center: mapCenter,
+            zoom: 5.6
+        });
+    }
 }
 
 const app = new App();
